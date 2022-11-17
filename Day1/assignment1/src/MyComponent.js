29232032613191173465a665716dac9d66ff91b3
ajay <ap221882@gmail.com>
2022-11-17 08:56:35 +0530

    checking for object in props

--- Day1/assignment1/src/MyComponent.js
+++ Day1/assignment1/src/MyComponent.js
@@ -2,6 +2,10 @@ const createReactElement = React.createElement;
 const createReactRoot = ReactDOM.createRoot;
 const rootDOMElement = document.querySelector("#root");
 // console.log(createReactRoot);
-const element = createReactElement("h1", {}, "Hello World");
+const element = createReactElement(
+  "h1",
+  { props: { name: "Ajay" } },
+  "Hello World"
+);
 const rootOfApplication = createReactRoot(rootDOMElement);
 rootOfApplication.render(element);
