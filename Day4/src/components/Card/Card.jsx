1594188ad71d946dfa51a71a6f86ecde96a16ec7
ajay <ap221882@gmail.com>
2022-11-28 23:50:57 +0530

    fix: renamed data in Card Component

--- Day4/src/components/Card/Card.jsx
+++ Day4/src/components/Card/Card.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import * as classes from "./Card.styles.module.css";
 import Data from "../../pages/HomePage/AvengersData";
 
-const Card = ({ singleData }) => {
+const Card = ({ singleData: data }) => {
   return (
     <section className={classes.card__container}>
       <article className={classes.img__container}>
         <img
           src={
-            singleData?.imageSrc
-              ? singleData.imageSrc
+            data?.imageSrc
+              ? data.imageSrc
               : "https://www.seekpng.com/png/detail/115-1150053_avatar-png-transparent-png-royalty-free-default-user.png"
           }
-          alt={`${singleData.name}'s Image`}
+          alt={`${data.name}'s Image`}
         />
         <div className={classes.overlay}></div>
       </article>
       <article className={classes.info__container}>
-        <h3>{singleData.name}</h3>
+        <h3>{data.name}</h3>
         <div className={classes.separator}></div>
-        <h4>{singleData.place}</h4>
-        <p className={classes.company}>{singleData.company}</p>
-        <p>{singleData.designation}</p>
+        <h4>{data.place}</h4>
+        <p className={classes.company}>{data.company}</p>
+        <p>{data.designation}</p>
       </article>
     </section>
   );
