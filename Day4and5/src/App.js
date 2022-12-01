1ab46a8ccca72f4ff7ce220b217dcbd08b786675
ajay <ap221882@gmail.com>
2022-12-01 19:35:30 +0530

    feat: structured the folders and components

--- Day4and5/src/App.js
+++ Day4and5/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 import "./App.styles.css";
-import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 
 const app = () => {
   return (
     <>
-      <Header />
       <HomePage />
     </>
   );
--- Day4and5/src/pages/HomePage/HomePage.jsx
+++ Day4and5/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import Cards from "../../components/Cards/Cards";
+import Header from "../../components/Header/Header";
 
 const homePage = () => (
   <>
+    <Header />
     <Cards />
   </>
 );
