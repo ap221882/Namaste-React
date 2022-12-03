eda44b7d5798109c83d8feb041b504f89d750d62
ajay <ap221882@gmail.com>
2022-12-03 12:23:10 +0530

    feat: added basic configurations

---
+++ Day6/github-api-project-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import Card from "./components/Card/Card";
+// import TestComponent from "./components/TestComponent/TestComponent";
+import { Card } from "./components";
+
+function App() {
+  return (
+    <h1>
+      <Card />
+    </h1>
+  );
+}
+
+export default App;
---
+++ Day6/github-api-project-ts/src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Card = (props: Props) => {
+  return <div>Card</div>;
+};
+
+export default Card;
