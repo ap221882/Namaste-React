77e60c59bf4368b65aa741d85a6ee67cd2f4105c
ajay <ap221882@gmail.com>
2022-12-10 12:31:24 +0530

    feat: added Error Page

---
+++ miscellaneous/react-router-docs-tutorials/src/error-page.jsx
@@ -0,0 +1,19 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id='error-page'>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occured.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <p>
+        <i>{error.status}</i>
+      </p>
+    </div>
+  );
+}
--- miscellaneous/react-router-docs-tutorials/src/main.jsx
+++ miscellaneous/react-router-docs-tutorials/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
 const myAppRouter = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
