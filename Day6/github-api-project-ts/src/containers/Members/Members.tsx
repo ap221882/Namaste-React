67421d33a212eb50c0a3e0c8d7cd2e97142e2587
ajay <ap221882@gmail.com>
2022-12-11 01:30:59 +0530

    feat: solution2 - more suitable

--- Day6/github-api-project-ts/src/containers/Members/Members.tsx
+++ Day6/github-api-project-ts/src/containers/Members/Members.tsx
@@ -9,25 +9,40 @@ import { AvengersIds } from "./membersData";
 const Members = () => {
   const [membersData, setMembersData] = useState<IUser[]>([]);
 
+  const fetchData = () => {
+    return AvengersIds.map(async (avengerId) => {
+      const res = await getUserData(avengerId.id);
+      return res.data;
+    });
+  };
+
   useEffect(() => {
-    const avengerData: IUser[] = [];
-    const getData = () => {
-      AvengersIds.forEach(async (avengerId, index) => {
-        try {
-          const response = await getUserData(avengerId.id);
-          avengerData.push(response.data);
-          if (index + 1 === AvengersIds.length) {
-            setMembersData([...avengerData]);
-          }
-        } catch (err) {
-          alert(err);
-        }
-      });
-    };
-    getData();
-    // console.log(avengerData, "avengerData");
-    // setMembersData([...avengerData]);
+    const promises = fetchData();
+    Promise.all(promises).then((data) => {
+      console.log(data, "data");
+      setMembersData(data);
+    });
   }, []);
+
+  // useEffect(() => {
+  //   const promises = fetchData();
+  //   const getData = () => {
+  //     AvengersIds.forEach(async (avengerId, index) => {
+  //       try {
+  //         const response = await getUserData(avengerId.id);
+  //         avengerData.push(response.data);
+  //         if (index + 1 === AvengersIds.length) {
+  //           setMembersData([...avengerData]);
+  //         }
+  //       } catch (err) {
+  //         alert(err);
+  //       }
+  //     });
+  //   };
+  //   getData();
+  //   // console.log(avengerData, "avengerData");
+  //   // setMembersData([...avengerData]);
+  // }, []);
 
   console.log(membersData, "membersData");
 
