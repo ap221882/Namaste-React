70ef2dcf8276c5e92393b34bfdd238bf1c4d1dc3
ajay <ap221882@gmail.com>
2022-12-12 19:18:41 +0530

    feat: conditional rendering of loader in CBC

--- Day7n8/github-api-project-ts/src/containers/Members/Members.tsx
+++ Day7n8/github-api-project-ts/src/containers/Members/Members.tsx
@@ -5,29 +5,45 @@ import { Card } from "../../components";
 import { getUserData } from "../../services";
 import { IUser } from "../../types";
 import classes from "./member.styles.module.css";
+import Loader from "../../components/Loader/Loader";
 
-class Members extends Component<{}, { membersData: IUser[] }> {
-  state: { membersData: IUser[] } = { membersData: [] };
+class Members extends Component<{}, { membersData: IUser[]; loader: boolean }> {
+  state: { membersData: IUser[]; loader: boolean } = {
+    membersData: [],
+    loader: false,
+  };
 
   componentDidMount() {
     (async () => {
-      const promisesResponseArray = await getUserData();
-      this.setState({ membersData: promisesResponseArray });
+      try {
+        this.setState({ loader: true });
+        const promisesResponseArray = await getUserData();
+        this.setState({ membersData: promisesResponseArray });
+      } catch (err) {
+        alert(err);
+      } finally {
+        this.setState({ loader: false });
+      }
     })();
   }
 
   render() {
-    return (
-      <section className={classes.member__container}>
-        {this.state.membersData.map((member) => {
-          return (
-            <Link to={`/members/${member?.login}`}>
-              <Card key={member.id} avenger={member} />;
-            </Link>
-          );
-        })}
-      </section>
-    );
+    const isLoading = this.state.loader;
+    if (isLoading) {
+      return <Loader />;
+    } else {
+      return (
+        <section className={classes.member__container}>
+          {this.state.membersData.map((member) => {
+            return (
+              <Link to={`/members/${member?.login}`}>
+                <Card key={member.id} avenger={member} />;
+              </Link>
+            );
+          })}
+        </section>
+      );
+    }
   }
 }
 
