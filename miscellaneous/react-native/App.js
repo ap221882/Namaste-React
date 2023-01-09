59c26e5698b32dce1c1c655c32fb3b054b881ef2
ajay <ap221882@gmail.com>
2023-01-09 21:11:53 +0530

    feat: rendering a list

--- miscellaneous/react-native/App.js
+++ miscellaneous/react-native/App.js
@@ -2,6 +2,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 /**
  * * From react-navigation library used for routing
@@ -10,9 +11,10 @@ const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App",
     },
--- miscellaneous/react-native/src/screens/ListScreen.js
+++ miscellaneous/react-native/src/screens/ListScreen.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
+
+import studentData from "../../assets/data.json";
 
 const ListScreen = () => {
   return (
     <View>
       <Text>ListScreen</Text>
+      <FlatList
+        // horizontal // having horizontal scroll
+        // showsHorizontalScrollIndicator={false} // hiding horizontal scroll indicator
+        data={studentData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.textStyle}>
+              <Text>Name: {item.first_name}</Text>
+              <Text>Gender:{item.gender}</Text>
+              <Text>Email: {item.email}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
 
 export default ListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 20,
+  },
+});
