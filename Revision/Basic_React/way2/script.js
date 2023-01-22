a09b8b7ca83fc669370e4c3b4765eaa4f1037cbf
ajay <ap221882@gmail.com>
2023-01-22 19:23:14 +0530

    feat: added custom npm package to the project

--- Revision/Basic_React/way2/script.js
+++ Revision/Basic_React/way2/script.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const countryData = require('country-name-library');
 
-console.log(React);
-console.log(ReactDOM);
+console.log(React, 'React Global Object');
+console.log(ReactDOM, 'ReactDOM Global Object');
+console.log(countryData, 'countryData');
+
+const KeysArrayReact = Object.keys(React);
+const KeysArrayReactDOM = Object.keys(ReactDOM);
+console.log(KeysArrayReact.length, KeysArrayReactDOM.length);
