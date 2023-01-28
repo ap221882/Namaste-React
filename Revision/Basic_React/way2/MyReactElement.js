ad411c9e3725fbf973e82bda39e88f674d1b6208
ajay <ap221882@gmail.com>
2023-01-28 11:27:14 +0530

    feat: react composition

--- Revision/Basic_React/way2/MyReactElement.js
+++ Revision/Basic_React/way2/MyReactElement.js
@@ -1,5 +1,4 @@
 import React from './React';
-import ReactDOM from 'react-dom';
 
 // console.table(React.createElement);
 // console.log(React.createElement(() => {}));
@@ -9,15 +8,29 @@ import ReactDOM from 'react-dom';
 // console.log(React.createElement({ ajay: 'pathak' }));
 // console.log(React.createElement(null));
 // console.log(React.createElement(Symbol('Ajay')));
-console.log(
-  React.createElement(
-    undefined,
-    { __self: 'Ajay', key: 'Ajay' },
-    { __self: 'Ajay', key: 'Ajay' },
-    { __self: 'Ajay', key: 'Ajay' }
-  )
-);
+// console.log(
+//   React.createElement(
+//     undefined,
+//     { __self: 'Ajay', key: 'Ajay' },
+//     { __self: 'Ajay', key: 'Ajay' },
+//     { __self: 'Ajay', key: 'Ajay' }
+//   )
+// );
 
-const ReactElement = React.createElement('h1', { alt: 'logo' }, <h1>Hello</h1>);
+// const ReactElement = React.createElement('h1', { alt: 'logo' }, <h1>Hello</h1>);
 
 // console.log(ReactElement);
+
+const ParentElement = function ({ children }) {
+  return React.createElement('h1', { alt: 'logo' }, <h1>`${children}`</h1>);
+};
+
+console.log(ParentElement, 'ParentElement');
+
+console.log(
+  React.createElement(
+    ParentElement,
+    { key: 'unique-value', children: 'Hi!' },
+    'hello'
+  )
+);
