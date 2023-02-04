b71c46fcab835c8991d489589771fc36aca08b26
ajayp <ajayp@naehas.com>
2023-02-04 19:08:39 +0530

    feat: invoking the JSX

---
+++ Revision/stateInReact/component.js
@@ -0,0 +1,42 @@
+function App() {
+  const counter = <Counter />;
+
+  return (
+    <div>
+      {counter}
+      {counter}
+    </div>
+  );
+}
+
+function Counter() {
+  const [score, setScore] = React.useState(0);
+  const [hover, setHover] = React.useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>Add one</button>
+    </div>
+  );
+}
+
+const reactDOMRoot = ReactDOM.createRoot(document.getElementById('react-root'));
+console.log(<App />, '<App />');
+const app = <App />;
+console.dir(app.type, 'type');
+console.log(app.type, 'type');
+console.log(app.type(), 'type invoked');
+console.log(app.type().props.children, 'app.props.children');
+console.table(app.type().props.children[0].type, 'app.type().props.children');
+
+reactDOMRoot.render(<App />);
